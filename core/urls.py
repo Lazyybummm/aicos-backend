74c8a4095bc9ca1f5d1fb72721376129b7bb5d5d
vivek <vivekpat931@gmail.com>
2026-06-29 14:09:52 +0530

    add: route for leave management

--- core/urls.py
+++ core/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/leave-management/', include('leave_management.urls')),
 ]
 
 # Serve media files in development (for local storage fallback)
